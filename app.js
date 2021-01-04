910ebfae58989571234f8e8c42e6e5448a44a47e
Rohit Pednekar <rohitpednekar95@gmail.com>
2021-01-05 00:31:46 +0530

    Chat window and room created on server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const path = require('path');
+const session = require('express-session');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
@@ -11,6 +12,18 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+    secret: '540e7760-7a36-40b7-8c5e-291840508fd6',  
+    name: "ChatApp",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        maxAge: 600000
+    }
+  }))
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -26,5 +39,9 @@ http.listen(port, function() {
 
 io.sockets.on('connection', function (socket) {
     console.log('this is connected user')
-  });
+    socket.on('createRoom', function(room) {
+        console.log(room)
+        socket.join(room);
+    });
+});
 
--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,14 +1,22 @@
 var socket = io.connect('http://localhost:3000');
-    socket.on('receive', function (message) {
-      console.log('received %s', message);
-      document.querySelector('.received-message').innerText = message;
-    });
 
 
-
-    function send(input) {
-      console.log(input.value)
-      var value = input.value;
-      console.log('Sending Client Message: %s to Server', value);
-      socket.emit('messageChange', {message: value});
+    function createRoom() {
+        var person = prompt("Please enter your name:")
+        if (person == null || person == "" || person == undefined) {
+            alert('Please enter name')
+        } 
+        if (person != null && person != "" && person != undefined) {
+            socket.emit('createRoom', `${person}${generateGUID()}`);
+            socket.emit('messageChange', {message: value});
+        }    
     }
+
+    function generateGUID() {
+        return 'xxxxxxxx-4xxx-xxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+    
